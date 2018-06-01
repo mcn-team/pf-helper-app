73700a3c512fbf6802b16204a294a7c5ed38f0ce
Matthias Brunet <matthias.brunet@viseo.com>
2018-06-01 08:55:23 +0200

    basic navbar added

--- src/app/components/app/app.js
+++ src/app/components/app/app.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import { Home } from '../pages/home/index'
 import About from '../pages/about/index'
 
 import './app.css';
 import { rootContainerStyle } from './app.styles';
 
+// TODO: Transform header to navbar component with redux state
 export const App = () => (
     <div>
         <header>
-            <Link to="/">Home</Link>
-            <Link to="/about-us">About</Link>
+            <nav className="navbar" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <NavLink to="/" className="navbar-item" activeClassName="nav-active" exact>Home</NavLink>
+                </div>
+                <div className="navbar-menu">
+                    <div className="navbar-start">
+                        <NavLink to="/about-us" className="navbar-item" activeClassName="nav-active" exact>About</NavLink>
+                    </div>
+
+                    <div className="navbar-end">
+                    </div>
+                </div>
+
+            </nav>
         </header>
 
         <main className="columns">
             <div className="column is-8 is-offset-2">
                 <div style={ rootContainerStyle }>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/about-us" component={About} />
+                    <Route exact path="/" component={ Home } />
+                    <Route exact path="/about-us" component={ About } />
                 </div>
             </div>
         </main>
